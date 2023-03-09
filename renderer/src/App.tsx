1ba177b990db5b8848bacf8d3104fbd139cc12f6
10tera <tyyt151123@gmail.com>
2023-03-09 23:47:51 +0900

    emotionの導入

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import {ThemeProvider, createTheme} from "@mui/material/styles";
 import {Routes, Route} from "react-router-dom";
 import Todo from "./pages/Todo";
 import Error404 from "./pages/Error404";
 
 const App = () => {
+    const theme = createTheme({
+        palette: {
+            mode: "light",
+            primary: {
+                main: "#006a69",
+                light: "#006a69",
+                dark: "#4ddad8",
+            },
+            secondary: {
+                main: "#4a6362",
+                light: "#4a6362",
+                dark: "#b0cccb"
+            },
+            error: {
+                main: "#ba1a1a",
+                light: "#ba1a1a",
+                dark: "#ffb4ab"
+            }
+        }
+    });
     return (
         <Routes>
             <Route path={"/"} element={<Todo />} />
